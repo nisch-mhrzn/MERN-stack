a4566f7dfbe4b9cf3024977d0801264a4ccda2b5
Nischal <nischal.maharjan1233@gmail.com>
2024-11-14 10:27:06 +0545

    used dotenv for URI

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const URI = "mongodb://127.0.0.1:27017/mern_admin";
-
+// const URI = "mongodb://127.0.0.1:27017/mern_admin";
+const URI = process.env.MONGODB_URI;
 // mongoose.connect(URI);
 const connectDb = async() =>{
     try{
