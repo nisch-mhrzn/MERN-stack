7d0117965b5be838248c9c1c5a2172f8b034cdaf
Nischal <nischal.maharjan1233@gmail.com>
2024-11-14 10:28:01 +0545

    used dotenv for URI

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // make server neat and clean
+require('dotenv').config();//to use dotenv
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
