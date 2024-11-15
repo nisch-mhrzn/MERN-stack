c8a3dc45c56d342dbd89103984f22147a96936c7
Nischal <nischal.maharjan1233@gmail.com>
2024-11-15 22:13:22 +0545

    registration and login form validation using ZOD

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -4,15 +4,15 @@ const User = require("../models/user-config");
 const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
-  //Try and catch block so we use async
   try {
     console.log(req.body);
-    res.status(200).json({ message: req.body });
+    res.status(200).json({ message: "Home endpoint hit", data: req.body });
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
+    console.error("Error in home endpoint:", error);
+    res.status(500).json({ error: "Server Error" });
   }
 };
+
 
 const register = async (req, res) => {
   try {
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -3,10 +3,11 @@
 const express = require("express");
 const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
-
+const signupSchema = require("../validators/auth-validators")
+const validate = require("../middlewares/validate-middleware");
 router.route("/").get(authcontrollers.home);
 
-router.route("/register").post(authcontrollers.register);//post goes and adds to database
+router.route("/register").post (validate(signupSchema), authcontrollers.register);//post goes and adds to database
 router.route("/login").post(authcontrollers.login);//post goes and adds to database
 
 
