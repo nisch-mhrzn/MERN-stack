adf7cad384cf223f847320e89f700c05336d9a31
Nischal <nischal.maharjan1233@gmail.com>
2024-11-15 22:14:12 +0545

    registration and login form validation using ZOD

---
+++ server/middlewares/validate-middleware.js
@@ -0,0 +1,17 @@
+// to check registration and zod details are same or not
+
+//parseAsync
+
+const validate = (schema) => async (req, res, next) => {
+  try {
+    const parseBody = await schema.parseAsync(req.body);
+    req.body = parseBody;
+    next();
+  } catch (err) {
+      const message = err.errors[0].message;
+      console.error(message);
+    res.status(400).json({ msg: message});
+  }
+};
+
+module.exports = validate;
