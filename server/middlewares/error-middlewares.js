e3fb7a4cd9283dc6f0bef94f8d8cd64ecaf8dbd8
Nischal <nischal.maharjan1233@gmail.com>
2024-11-16 21:06:09 +0545

    error middleware implementation

---
+++ server/middlewares/error-middlewares.js
@@ -0,0 +1,10 @@
+//you have to call next(err) no matter what
+const errorMiddleware =(err,req,res,next) => {
+    const status =err.status || 500;
+    const message = err.message || "Backend Error";
+    const extraDetails = err.extraDetails || "Error from backend"
+    return res.status(status).json({message,extraDetails})
+
+};
+
+module.exports = errorMiddleware;
